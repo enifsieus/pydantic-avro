37b4444a38cfc83f6aa04674518150672bf12ecd
Mike Everson <mike.everson@appcensus.io>
2023-02-09 15:44:54 -0500

    finished first pass, need tests

--- src/pydantic_avro/avro_to_graphql.py
+++ src/pydantic_avro/avro_to_graphql.py
@@ -15,7 +15,6 @@ def avsc_to_graphql(schema: dict) -> str:
 
     def get_graphql_type(t: Union[str, dict]) -> str:
         """Returns graphql type for given avro type."""
-        ##todo(mje): Do we want to add support for GraphQL ID scalar? JSONObject?
         optional = False
         if isinstance(t, str):
             if t == "string":
@@ -37,11 +36,7 @@ def avsc_to_graphql(schema: dict) -> str:
                 c.remove("null")
                 py_type = get_graphql_type(c[0])
             else:
-                ##todo(mje): Unions can't be inline, so we need to generate a union name
-                ##eg. union SearchResult = Conference | Festival | Concert | Venue
-                ##then py_type = SearchResult
-                py_type = f"Union[{','.join([ 'None' if e == 'null' else get_graphql_type(e) for e in t])}]"
-        ##todo(mje): Review all of these https://the-guild.dev/graphql/scalars
+                py_type = union_type_to_graphql(t)
         elif t.get("logicalType") == "uuid":
             py_type = "UUID"
         elif t.get("logicalType") == "decimal":
@@ -53,19 +48,8 @@ def avsc_to_graphql(schema: dict) -> str:
         elif t.get("logicalType") == "date":
             py_type = "Date"
         elif t.get("type") == "enum":
-            ##todo(mje): Similar to Union, Enum can't be inline, need to generate a new enum name,
-            ##eg. enum Episode {
-            # NEWHOPE
-            # EMPIRE
-            # JEDI}
-            ##then py_type = Episode
-            enum_name = t.get("name")
-            if enum_name not in classes:
-                enum_class = f"class {enum_name}(str, Enum):\n"
-                for s in t.get("symbols"):
-                    enum_class += f'    {s} = "{s}"\n'
-                classes[enum_name] = enum_class
-            py_type = enum_name
+            enum_type_to_graphql(t)
+            py_type = t.get("name")
         elif t.get("type") == "string":
             py_type = "String"
         elif t.get("type") == "array":
@@ -88,40 +72,45 @@ def avsc_to_graphql(schema: dict) -> str:
         else:
             return f"{py_type}!"
 
+    def union_type_to_graphql(t: list) -> str:
+        """Convert a single avro Union type to a graphql Union"""
+        name = "".join(t)
+        current = f'union {name} = {"|".join(t)}'
+        classes[name] = current
+        return name
+
+    def enum_type_to_graphql(schema: dict) -> str:
+        """Convert a single avro Enum type to a graphql Enum"""
+        name = schema["name"]
+        current = f"enum {name} {{\n"
+
+        if len(schema["types"]) > 0:
+            for symbol in schema["symbol"]:
+                current += f'    "{symbol}"\n'
+
+        current += "}\n"
+        classes[name] = current
+
     def record_type_to_graphql(schema: dict):
-        """Convert a single avro record type to a pydantic class"""
+        """Convert a single avro record type to a graphql Type"""
         name = schema["name"]
         current = f"type {name} "
 
         if len(schema["fields"]) > 0:
-            # todo(mje): We need some way of identifying Keys for types for federation
-            # Naive - if one of the fields is named id then use that as the Key
+            # todo(mje): Naive federation logic - if there is a  field named id then use that as the Entity Key
             field = next((x for x in schema["fields"] if x["name"] == "id"), None)
             if field is not None:
                 key_name = field["name"]
-                current += f"@key(fields: \"{key_name}\") "
+                current += f'@key(fields: "{key_name}") '
 
-            current += "{\n"
+        current += "{\n"
 
-            for field in schema["fields"]:
-                n = field["name"]
-                t = get_graphql_type(field["type"])
-                current += f"    {n}: {t}\n"
+        for field in schema["fields"]:
+            n = field["name"]
+            t = get_graphql_type(field["type"])
+            current += f"    {n}: {t}\n"
 
-                # Defaults aren't supported by graphQL types - instead they must be specified in the query
-                # https://github.com/graphql/graphql-js/issues/345
-                # default = field.get("default")
-                # if "default" not in field:
-                # current += f"    {n}: {t}\n"
-                # elif isinstance(default, (bool, type(None))):
-                #     current += f"    {n}: {t} = {default}\n"
-                # else:
-                #     current += f"    {n}: {t} = {json.dumps(default)}\n"
-            # if len(schema["fields"]) == 0:
-            #     current += "    pass\n"
-
-            current += "}\n"
-
+        current += "}\n"
         classes[name] = current
 
     record_type_to_graphql(schema)
