1bd3cf3da66c705965bf555d2b8f58f63fa9f475
Mike Everson <mike.everson@appcensus.io>
2023-02-09 15:21:49 -0500

    fleshing out graphql logic

--- src/pydantic_avro/avro_to_graphql.py
+++ src/pydantic_avro/avro_to_graphql.py
@@ -2,7 +2,7 @@ import json
 from typing import Optional, Union
 
 
-def avsc_to_pydantic(schema: dict) -> str:
+def avsc_to_graphql(schema: dict) -> str:
     """Generate python code of pydantic of given Avro Schema"""
     if "type" not in schema or schema["type"] != "record":
         raise AttributeError("Type not supported")
@@ -13,18 +13,19 @@ def avsc_to_pydantic(schema: dict) -> str:
 
     classes = {}
 
-    def get_python_type(t: Union[str, dict]) -> str:
-        """Returns python type for given avro type"""
+    def get_graphql_type(t: Union[str, dict]) -> str:
+        """Returns graphql type for given avro type."""
+        ##todo(mje): Do we want to add support for GraphQL ID scalar? JSONObject?
         optional = False
         if isinstance(t, str):
             if t == "string":
-                py_type = "str"
+                py_type = "String"
             elif t == "long" or t == "int":
-                py_type = "int"
+                py_type = "Int"
             elif t == "boolean":
-                py_type = "bool"
+                py_type = "Boolean"
             elif t == "double" or t == "float":
-                py_type = "float"
+                py_type = "Float"
             elif t in classes:
                 py_type = t
             else:
@@ -34,20 +35,30 @@ def avsc_to_pydantic(schema: dict) -> str:
                 optional = True
                 c = t.copy()
                 c.remove("null")
-                py_type = get_python_type(c[0])
+                py_type = get_graphql_type(c[0])
             else:
-                py_type = f"Union[{','.join([ 'None' if e == 'null' else get_python_type(e) for e in t])}]"
+                ##todo(mje): Unions can't be inline, so we need to generate a union name
+                ##eg. union SearchResult = Conference | Festival | Concert | Venue
+                ##then py_type = SearchResult
+                py_type = f"Union[{','.join([ 'None' if e == 'null' else get_graphql_type(e) for e in t])}]"
+        ##todo(mje): Review all of these https://the-guild.dev/graphql/scalars
         elif t.get("logicalType") == "uuid":
             py_type = "UUID"
         elif t.get("logicalType") == "decimal":
             py_type = "Decimal"
         elif t.get("logicalType") == "timestamp-millis" or t.get("logicalType") == "timestamp-micros":
-            py_type = "datetime"
+            py_type = "DateTime"
         elif t.get("logicalType") == "time-millis" or t.get("logicalType") == "time-micros":
-            py_type = "time"
+            py_type = "Time"
         elif t.get("logicalType") == "date":
-            py_type = "date"
+            py_type = "Date"
         elif t.get("type") == "enum":
+            ##todo(mje): Similar to Union, Enum can't be inline, need to generate a new enum name,
+            ##eg. enum Episode {
+            # NEWHOPE
+            # EMPIRE
+            # JEDI}
+            ##then py_type = Episode
             enum_name = t.get("name")
             if enum_name not in classes:
                 enum_class = f"class {enum_name}(str, Enum):\n"
@@ -56,56 +67,74 @@ def avsc_to_pydantic(schema: dict) -> str:
                 classes[enum_name] = enum_class
             py_type = enum_name
         elif t.get("type") == "string":
-            py_type = "str"
+            py_type = "String"
         elif t.get("type") == "array":
-            sub_type = get_python_type(t.get("items"))
-            py_type = f"List[{sub_type}]"
+            sub_type = get_graphql_type(t.get("items"))
+            py_type = f"[{sub_type}]"
         elif t.get("type") == "record":
-            record_type_to_pydantic(t)
+            record_type_to_graphql(t)
             py_type = t.get("name")
         elif t.get("type") == "map":
-            value_type = get_python_type(t.get("values"))
-            py_type = f"Dict[str, {value_type}]"
+            # There are no Dicts in graphql so JSONObject is our analog. We lose type enforcement though.
+            # value_type = get_graphql_type(t.get("values"))
+            py_type = "JSONObject"
         else:
             raise NotImplementedError(
                 f"Type {t} not supported yet, "
                 f"please report this at https://github.com/godatadriven/pydantic-avro/issues"
             )
         if optional:
-            return f"Optional[{py_type}]"
-        else:
             return py_type
+        else:
+            return f"{py_type}!"
 
-    def record_type_to_pydantic(schema: dict):
+    def record_type_to_graphql(schema: dict):
         """Convert a single avro record type to a pydantic class"""
         name = schema["name"]
-        current = f"class {name}(BaseModel):\n"
+        current = f"type {name} "
 
-        for field in schema["fields"]:
-            n = field["name"]
-            t = get_python_type(field["type"])
-            default = field.get("default")
-            if "default" not in field:
+        if len(schema["fields"]) > 0:
+            # todo(mje): We need some way of identifying Keys for types for federation
+            # Naive - if one of the fields is named id then use that as the Key
+            field = next((x for x in schema["fields"] if x["name"] == "id"), None)
+            if field is not None:
+                key_name = field["name"]
+                current += f"@key(fields: \"{key_name}\") "
+
+            current += "{\n"
+
+            for field in schema["fields"]:
+                n = field["name"]
+                t = get_graphql_type(field["type"])
                 current += f"    {n}: {t}\n"
-            elif isinstance(default, (bool, type(None))):
-                current += f"    {n}: {t} = {default}\n"
-            else:
-                current += f"    {n}: {t} = {json.dumps(default)}\n"
-        if len(schema["fields"]) == 0:
-            current += "    pass\n"
+
+                # Defaults aren't supported by graphQL types - instead they must be specified in the query
+                # https://github.com/graphql/graphql-js/issues/345
+                # default = field.get("default")
+                # if "default" not in field:
+                # current += f"    {n}: {t}\n"
+                # elif isinstance(default, (bool, type(None))):
+                #     current += f"    {n}: {t} = {default}\n"
+                # else:
+                #     current += f"    {n}: {t} = {json.dumps(default)}\n"
+            # if len(schema["fields"]) == 0:
+            #     current += "    pass\n"
+
+            current += "}\n"
 
         classes[name] = current
 
-    record_type_to_pydantic(schema)
+    record_type_to_graphql(schema)
 
     file_content = """
-from datetime import date, datetime, time
-from decimal import Decimal
-from enum import Enum
-from typing import List, Optional, Dict, Union
-from uuid import UUID
+extend schema @link(url: "https://specs.apollo.dev/federation/v2.0", import: ["@key", "@shareable"])
 
-from pydantic import BaseModel
+scalar Date
+scalar Decimal
+scalar DateTime
+scalar JSONObject
+scalar Time
+scalar UUID
 
 
 """
@@ -117,7 +146,7 @@ from pydantic import BaseModel
 def convert_graphql(avsc_path: str, output_path: Optional[str] = None):
     with open(avsc_path, "r") as fh:
         avsc_dict = json.load(fh)
-    file_content = avsc_to_pydantic(avsc_dict)
+    file_content = avsc_to_graphql(avsc_dict)
     if output_path is None:
         print(file_content)
     else:
---
+++ tests/test_to_graphql.py
@@ -0,0 +1,259 @@
+# from pydantic_avro.avro_to_graphql import avsc_to_graphql
+#
+#
+# def test_avsc_to_pydantic_empty():
+#     pydantic_code = avsc_to_pydantic({"name": "Test", "type": "record", "fields": []})
+#     assert "class Test(BaseModel):\n    pass" in pydantic_code
+#
+#
+# def test_avsc_to_pydantic_primitive():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "name": "Test",
+#             "type": "record",
+#             "fields": [
+#                 {"name": "col1", "type": "string"},
+#                 {"name": "col2", "type": "int"},
+#                 {"name": "col3", "type": "long"},
+#                 {"name": "col4", "type": "double"},
+#                 {"name": "col5", "type": "float"},
+#                 {"name": "col6", "type": "boolean"},
+#             ],
+#         }
+#     )
+#     assert (
+#         "class Test(BaseModel):\n"
+#         "    col1: str\n"
+#         "    col2: int\n"
+#         "    col3: int\n"
+#         "    col4: float\n"
+#         "    col5: float\n"
+#         "    col6: bool" in pydantic_code
+#     )
+#
+#
+# def test_avsc_to_pydantic_map():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "name": "Test",
+#             "type": "record",
+#             "fields": [
+#                 {"name": "col1", "type": {"type": "map", "values": "string", "default": {}}},
+#             ],
+#         }
+#     )
+#     assert "class Test(BaseModel):\n" "    col1: Dict[str, str]" in pydantic_code
+#
+#
+# def test_avsc_to_pydantic_map_nested_object():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "name": "Test",
+#             "type": "record",
+#             "fields": [
+#                 {
+#                     "name": "col1",
+#                     "type": {
+#                         "type": "map",
+#                         "values": {"type": "record", "name": "Nested", "fields": [{"name": "col1", "type": "string"}]},
+#                         "default": {},
+#                     },
+#                 },
+#             ],
+#         }
+#     )
+#     assert "class Test(BaseModel):\n" "    col1: Dict[str, Nested]" in pydantic_code
+#     assert "class Nested(BaseModel):\n" "    col1: str" in pydantic_code
+#
+#
+# def test_avsc_to_pydantic_map_nested_array():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "name": "Test",
+#             "type": "record",
+#             "fields": [
+#                 {
+#                     "name": "col1",
+#                     "type": {
+#                         "type": "map",
+#                         "values": {
+#                             "type": "array",
+#                             "items": "string",
+#                         },
+#                         "default": {},
+#                     },
+#                 },
+#             ],
+#         }
+#     )
+#     assert "class Test(BaseModel):\n" "    col1: Dict[str, List[str]]" in pydantic_code
+#
+#
+# def test_avsc_to_pydantic_logical():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "name": "Test",
+#             "type": "record",
+#             "fields": [
+#                 {
+#                     "name": "col1",
+#                     "type": {"type": "int", "logicalType": "date"},
+#                 },
+#                 {
+#                     "name": "col2",
+#                     "type": {"type": "long", "logicalType": "time-micros"},
+#                 },
+#                 {
+#                     "name": "col3",
+#                     "type": {"type": "long", "logicalType": "time-millis"},
+#                 },
+#                 {
+#                     "name": "col4",
+#                     "type": {"type": "long", "logicalType": "timestamp-micros"},
+#                 },
+#                 {
+#                     "name": "col5",
+#                     "type": {"type": "long", "logicalType": "timestamp-millis"},
+#                 },
+#             ],
+#         }
+#     )
+#     assert (
+#         "class Test(BaseModel):\n"
+#         "    col1: date\n"
+#         "    col2: time\n"
+#         "    col3: time\n"
+#         "    col4: datetime\n"
+#         "    col5: datetime" in pydantic_code
+#     )
+#
+#
+# def test_avsc_to_pydantic_complex():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "name": "Test",
+#             "type": "record",
+#             "fields": [
+#                 {
+#                     "name": "col1",
+#                     "type": {
+#                         "name": "Nested",
+#                         "type": "record",
+#                         "fields": [],
+#                     },
+#                 },
+#                 {
+#                     "name": "col2",
+#                     "type": {
+#                         "type": "array",
+#                         "items": "int",
+#                     },
+#                 },
+#                 {
+#                     "name": "col3",
+#                     "type": {
+#                         "type": "array",
+#                         "items": "Nested",
+#                     },
+#                 },
+#             ],
+#         }
+#     )
+#     assert (
+#         "class Test(BaseModel):\n"
+#         "    col1: Nested\n"
+#         "    col2: List[int]\n"
+#         "    col3: List[Nested]\n" in pydantic_code
+#     )
+#
+#     assert "class Nested(BaseModel):\n    pass\n" in pydantic_code
+#
+#
+# def test_default():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "name": "Test",
+#             "type": "record",
+#             "fields": [
+#                 {"name": "col1", "type": "string", "default": "test"},
+#                 {"name": "col2_1", "type": ["null", "string"], "default": None},
+#                 {"name": "col2_2", "type": ["string", "null"], "default": "default_str"},
+#                 {"name": "col3", "type": {"type": "map", "values": "string"}, "default": {"key": "value"}},
+#                 {"name": "col4", "type": "boolean", "default": True},
+#                 {"name": "col5", "type": "boolean", "default": False},
+#             ],
+#         }
+#     )
+#     assert (
+#         "class Test(BaseModel):\n"
+#         '    col1: str = "test"\n'
+#         "    col2_1: Optional[str] = None\n"
+#         '    col2_2: Optional[str] = "default_str"\n'
+#         '    col3: Dict[str, str] = {"key": "value"}\n'
+#         "    col4: bool = True\n"
+#         "    col5: bool = False\n" in pydantic_code
+#     )
+#
+#
+# def test_enums():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "name": "Test",
+#             "type": "record",
+#             "fields": [
+#                 {"name": "c1", "type": {"type": "enum", "symbols": ["passed", "failed"], "name": "Status"}},
+#             ],
+#         }
+#     )
+#
+#     assert "class Test(BaseModel):\n" "    c1: Status" in pydantic_code
+#
+#     assert "class Status(str, Enum):\n" '    passed = "passed"\n' '    failed = "failed"' in pydantic_code
+#
+#
+# def test_enums_reuse():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "name": "Test",
+#             "type": "record",
+#             "fields": [
+#                 {"name": "c1", "type": {"type": "enum", "symbols": ["passed", "failed"], "name": "Status"}},
+#                 {"name": "c2", "type": "Status"},
+#             ],
+#         }
+#     )
+#
+#     assert "class Test(BaseModel):\n" "    c1: Status\n" "    c2: Status" in pydantic_code
+#
+#     assert "class Status(str, Enum):\n" '    passed = "passed"\n' '    failed = "failed"' in pydantic_code
+#
+# def test_unions():
+#     pydantic_code = avsc_to_pydantic(
+#         {
+#             "type" : "record",
+#             "name" : "Test",
+#             "fields" : [
+#                 {
+#                     "name" : "a_union",
+#                     "type" : [
+#                         "null", "long", "string",
+#                         {
+#                             "type" : "record",
+#                             "name" : "ARecord",
+#                             "fields" : [
+#                                 {
+#                                     "name" : "values",
+#                                     "type" : {
+#                                         "type" : "map",
+#                                         "values" : "string"
+#                                     }
+#                                 }
+#                             ]
+#                         }
+#                     ]
+#                 }
+#             ]
+#         }
+#     )
+#
+#     assert "a_union: Union[None,int,str,ARecord]" in pydantic_code
